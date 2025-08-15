70ddf74fd9825fc26b270771a126ac7bbe09218d
Julian Geiger <julian.geiger@psi.ch>
2025-08-15 09:52:11 +0200

    add files

---
+++ lua/community.lua
@@ -0,0 +1,13 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  -- ... import any community contributed plugins here
+}
+  -- import/override with your plugins folder
+
