a3f7192f487b7ed1dbe531126dbc31fc601a0798
Julian Geiger <julian.geiger@gmx.net>
2025-09-15 15:22:33 +0200

    Add files from astronvim v5

--- lua/community.lua
+++ lua/community.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -6,15 +8,5 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.git.neogit" },
-  { import = "astrocommunity.git.gist-nvim" },
-  -- { import = "astrocommunity.git.octo-nvim" },
-  { import = "astrocommunity.motion.flash-nvim" },
-  { import = "astrocommunity.test.neotest" },
-  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  -- ... import any community contributed plugins here
-}
   -- import/override with your plugins folder
-
+}
