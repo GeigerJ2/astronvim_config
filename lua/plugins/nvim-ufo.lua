381c17df60610e6b64fa392f35615869c865c675
Julian Geiger <julian.geiger@gmx.net>
2025-09-15 17:17:42 +0200

    still trying to be able to show top-line of folded block

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,3 +1,6 @@
+-- File: lua/plugins/nvim-ufo.lua
+-- This configuration enables nvim-ufo and disables AstroNvim's built-in folding
+
 ---@type LazySpec
 return {
   "kevinhwang91/nvim-ufo",
@@ -67,6 +70,34 @@ return {
     close_fold_current_line_for_ft = {
       default = false, -- Don't auto-close folds on current line
     },
+    -- Custom fold text handler to show top line and number of folded lines
+    fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, { suffix, "MoreMsg" })
+      return newVirtText
+    end,
     preview = {
       mappings = {
         scrollB = "<C-B>",
@@ -75,22 +106,12 @@ return {
         scrollD = "<C-D>",
       },
     },
-    provider_selector = function(_, filetype, buftype)
-      local function handleFallbackException(bufnr, err, providerName)
-        if type(err) == "string" and err:match "UfoFallbackException" then
-          return require("ufo").getFolds(bufnr, providerName)
-        else
-          return require("promise").reject(err)
-        end
-      end
+    provider_selector = function(bufnr, filetype, buftype)
+      -- Return empty string for special buffer types
+      if buftype == "nofile" or buftype == "terminal" or buftype == "prompt" then return "" end
 
-      return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-        or function(bufnr)
-          return require("ufo")
-            .getFolds(bufnr, "lsp")
-            :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
-            :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
-        end
+      -- For normal files, prefer LSP first, then treesitter, then indent
+      return { "lsp", "treesitter" }
     end,
   },
 }
